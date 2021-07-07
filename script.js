403e4e83988174ba724abea691d42b4e58cde881
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-07-07 19:54:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function draw(){
 	background(random(255),random(255),random(255));
 	fill(255);
 	//circle(width/2,height/2,200);
-	text(mouseX,20,20);
+	text(mouseX+" "+mouseY,mouseX,mouseY);
 	//text(mouseX,mouseX+30,60);
 	
 }
