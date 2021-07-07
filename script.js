7cf67ca703cb5f9287d71c10ce02ced1b2e71752
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-07-07 20:05:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
 function setup(){ 
 	createCanvas(1200,800);
-	frameRate(1);
+
 }
 
 function draw(){
-	background(random(255),random(255),random(255));
-	fill(255);
-	//circle(width/2,height/2,200);
+	background(255);
+	fill(0);
+	textSize(60);
 	text(mouseX+" "+mouseY,mouseX,mouseY);
-	//text(mouseX,mouseX+30,60);
 	
 }
