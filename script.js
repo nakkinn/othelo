680f308ed5da02ff9887f93ff5a61c2d88e90023
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-07-08 00:36:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
+let img;
 
 function setup(){ 
-	createCanvas(windowWidth,windowHeight);
-	colorMode(HSB,800,100,100);
+	img=loadImage('cliff.
 }
 
 function draw(){
-	background("#f0f0f0");
-    noStroke();
-	fill(frameCount%800,100,80);
-	ellipse(mouseX,mouseY,400);
+
 }
