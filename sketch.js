eb6147a48644a4b68f7381e362c739d3f464722e
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-07-09 01:18:18 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,38 @@
 let img;
 let input;
+let button;
+let x,y;
+let c;
+let name='img';
 
 function preload(){
 	input=createFileInput(callback);
-	img=loadImage('moai.jpg');
+	img=loadImage("moai.png");
+	button=createButton("Save Image");
+	
+	x=img.width;
+	y=img.height;
+
 }
 
 function setup(){
-	createCanvas(screen.width,screen.height);
-	
+	c=createCanvas(screen.width,screen.height);
+	button.mousePressed(savebutton);
 }
 
 function draw(){
-	background(220);
-	image(img,0,0);
+	background(255);
+	
+	if(img.width!=x||img.height!=y){
+		x=img.width;
+		y=img.height;
+
+		resizeCanvas(x,y);
+	}
+
+
+	image(img,0,0)*0.9;
+	filter(GRAY);
 	
 }
 
@@ -21,8 +40,13 @@ function callback(file){
 
 	if(file.type=="image"){
 		img=loadImage(file.data); 
-		
+		name=file.name.slice(0,4)
 	}
-	
+
+}
+
+function savebutton(){
+	saveCanvas(c,'monochro'+name,'jpg')
+
 }
 
