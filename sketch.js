3d94bc70044af71c93ac8081ff6a77763a7118f9
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-07-09 19:02:25 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,52 @@
-function setup() {
-    let myNum = 10;
-    let myBool = false;
-    storeItem('key1', myNum); // key1 に 10 を保存します  
-    storeItem('key2', myBool); // key2 に false を保存します
-    print(getItem('key1'));
-    print(getItem('key2'));
-    clearStorage() ;        // クリアします 
-    print(getItem('key1')); // 再度、key1 を取得します -> NULL を表示
-    print(getItem('key2')); // 再度、key2 を取得します -> NULL を表示
-  }
+let img;
+let input;
+let button;
+let x,y;
+let c;
+let name='img';
+
+function preload(){
+	input=createFileInput(callback);
+	img=loadImage("");
+	button=createButton("Save Image");
+	
+	x=img.width;
+	y=img.height;
+
+}
+
+function setup(){
+	c=createCanvas(screen.width,screen.height);
+	button.mousePressed(savebutton);
+}
+
+function draw(){
+	background(255);
+	
+	if(img.width!=x||img.height!=y){
+		x=img.width;
+		y=img.height;
+
+		resizeCanvas(x,y);
+	}
+
+
+	image(img,0,0)*0.9;
+	filter(GRAY);
+	
+}
+
+function callback(file){
+
+	if(file.type=="image"){
+		img=loadImage(file.data); 
+		name=file.name.slice(0,4)
+	}
+
+}
+
+function savebutton(){
+	saveCanvas(c,'monochro'+name,'jpg')
+
+}
+
