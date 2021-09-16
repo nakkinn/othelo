ce829f0e8450cf8aabf4b4a827e8b1406c55b41e
nakkinn <81859514+nakkinn@users.noreply.github.com>
2021-09-16 22:54:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,277 +1,214 @@
-let dim;
-var conn;
-let inp,button;
-let url;
-let swi1=false,swi2=false,swi3=false,swi4=false;
-
-let w;
-let board=new Array(10);
-for(let i=0;i<10;i++){
-    board[i]=new Array(10);
-}
-let potato=1,tomato=2;
-let resp=0,rest=0;
-
-let peer;
+let block=new Array(17);
+for(let i=0;i<17;i++)   block[i]=new Array(17);
+let ts=60,rs=15,px=50,py=50,mode=0,c=6,r=6;
+let dir=[[[-1,0],[1,0]],[[0,-1],[0,1]]],col=['#3a57fd','#f14434'];
+let pnum=1,mem=2,start=true;
+let myturn,turn=0;
+let wall=4;
+let peer,room,id="";
 
 function setup(){
+    createCanvas(windowWidth,windowHeight);
 
-
-    createCanvas(200,200);
-    
-    
-    
-    peer= new Peer(makeid(),{
+    peer=new Peer({
         key: 'cf1155ef-ab9f-41a3-bd4a-b99c30cc0663',
-        debug: 3
-        
+        debug:1
     });
-
-    peer.on('open', () => {
-    
-        dim=createP(peer.id);
-        
-    });
-    
-    peer.on('connection', function(connection){
-    
-        conn = connection;
-    
-        conn.on("open", function() {
-            
+    peer.on('open',()=>{
+        id=peer.id;
+        room=peer.joinRoom("room",{
+            mode:'sfu'
         });
-     
-        conn.on("data", onRecvMessage);
-        
-        swi2=swi3=true;
-        start();
+        room.on('open',()=>{
+            pnum=room.members.length+1;
+            for(let i=2;i<15;i+=2)  block[i][16]=7;
+            if(pnum==1) myturn=true;
+            else    turn=1;
+        });
+        room.on('peerJoin',peerId=>{
+            console.log(peerId+"参加");
+        });
+        room.on('peerLeave',peerId=>{
+            console.log(peerId+"退出");
+        });
+        room.on('data',message=>{
+            console.log(message.data);
+            receive(message.data);
+        });
     });
 
-
-    inp=createInput('');
-    button=createButton("接続");
-    button.mousePressed(myInputEvent);
-    
-    url=createA('https://t.co/hCCMOMXzHu?amp=1', 'home');
-    url.position(30,500);
-    
+    for(let i=0;i<17;i++)   for(let j=0;j<17;j++)   block[i][j]=0;
 }
 
 function draw(){
-    if(swi1==false){
-        background('#aaeeff');
-        fill(0);
-        text("相手のidを入力してください",30,height/2);
-    }else    disp();
-    
-    
+    background(255);
 
-}
-
-function myInputEvent(){
-    
-    conn=peer.connect(inp.value());
-    conn.on("data", onRecvMessage);
-    
-    swi2=swi3=false;
-    start();
-}
-
-
-function onRecvMessage(data){
-    console.log(data);
-    let r,c;
-    r=Number(data.split(',')[0]);
-    c=Number(data.split(',')[1]);
-    flip(r,c,tomato);
-    if(data.split(',')[2]=='a') swi3=true;
-    if(data.split(',')[2]=='c'){
-        count();
-        swi4=true;
-    }
-    
-}
-
-function start(){
-    resizeCanvas(windowWidth,windowHeight);
-    w=min(width,height)*0.8;
-
-    swi1=true;
-    button.remove();
-    inp.remove();
-
-    for(let i=0;i<10;i++)   for(let j=0;j<10;j++){
-        board[i][j]=0;
-    }
-    board[5][5]=1;
-    board[4][4]=1;
-    board[5][4]=2;
-    board[4][5]=2;
-  
-    if(swi2){
-        potato=1;
-        tomato=2;
-    }else{
-        potato=2;
-        tomato=1;
-    }
-    url.position(20,windowHeight+60);
-}
-
-function mouseClicked(){
-    if(swi3==true&&swi4==false){
-        console.log(board);
-        let r,c;
-        if(mouseX>=w/8&&mouseX<w/8+w&&mouseY>=w/8&&mouseY<w/8+w){
-            r=int((mouseY-w/8)/(w/8))+1;
-            c=int((mouseX-w/8)/(w/8))+1;
-            if(judge(r,c,potato)){
-                flip(r,c,potato)
-                
-                if(enable(tomato)){
-                    swi3=false;
-                    conn.send(r+','+c+',a');
-                }else{
-                    conn.send(r+','+c+',b');
-                }
-                if(enable(potato)==false&&enable(tomato)==false){
-                    swi3=false;
-                    swi4=true;
-                    count();
-                    conn.send(r+','+c+',c');
-                }
-                
-    
+    let pc=-1,pr=-1;
+    for(let i=0;i<9;i++)    for(let j=0;j<9;j++){
+        if(mouseX>i*(ts+rs)+px&&mouseX<i*(ts+rs)+ts+px&&mouseY>j*(ts+rs)+py&&mouseY<j*(ts+rs)+ts+py){
+            if(block[i*2][j*2]==7){
+                pc=i*2;
+                pr=j*2;
             }
+            break;
         }
     }
-}
 
-function judge(r,c,s){
-    let y=[1,1,0,-1,-1,-1,0,1];
-    let x=[0,1,1,1,0,-1,-1,-1];
-    let result=0;
-
-    if(board[r][c]==0){
-        for(let i=0;i<8;i++){
-           for(let j=1;j<8;j++){
-               if(board[r+y[i]*j][c+x[i]*j]==0)   break;
-               if(board[r+y[i]*j][c+x[i]*j]==s){
-                   //for(let k=1;k<j;k++) board[r+y[i]*k][c+x[i]*k]=potato;
-                   result+=j-1;
-                   break;
-               }
-           }
-        }
-           if(result>0)    return true;
-        
-            else    return false;
-    }else   return false;
-        
-}
-
-function flip(r,c,s){
-    let y=[1,1,0,-1,-1,-1,0,1];
-    let x=[0,1,1,1,0,-1,-1,-1];
-    
-    for(let i=0;i<8;i++){
-        for(let j=1;j<8;j++){
-            if(board[r+y[i]*j][c+x[i]*j]==0)   break;
-            if(board[r+y[i]*j][c+x[i]*j]==s){
-                for(let k=1;k<j;k++) board[r+y[i]*k][c+x[i]*k]=s;
-                break;
+    for(let i=0;i<17;i++)   for(let j=0;j<17;j++)   if(block[i][j]==1)  block[i][j]=0;
+    if(pc==-1&&myturn&&start==false)    for(let i=0;i<8;i++)   for(let j=0;j<8;j++){
+        if(dist(mouseX,mouseY,ts+i*(ts+rs)+rs/2+px,ts+j*(ts+rs)+rs/2+py)<ts/3){
+            if(block[i*2+1][j*2+1]==0&&block[i*2+1+dir[mode][0][0]][j*2+1+dir[mode][0][1]]==0
+                &&block[i*2+1+dir[mode][1][0]][j*2+1+dir[mode][1][1]]==0&&wall>0){
+                block[i*2+1][j*2+1]=1;
+                block[i*2+1+dir[mode][0][0]][j*2+1+dir[mode][0][1]]=1;
+                block[i*2+1+dir[mode][1][0]][j*2+1+dir[mode][1][1]]=1;
             }
-        }
-    }
-    board[r][c]=s;
-    
-}
-
-function enable(s){
-    for(let i=1;i<9;i++)    for(let j=1;j<9;j++){
-        if(board[i][j]==0){
-            if(judge(i,j,s)) return true;
-        }
-    }
-    return false;
-}
-
-
-function disp(){
-    background(240);
-    
-    if(swi2)    fill(0);
-        else    fill(255);
-    circle(20,20,30);
-    
-    if(swi3){
-        noStroke();
-        fill('#dd5555');
-        square(w/8-w/50,w/8-w/50,w+w/25);
-        stroke(0);
-    }
-     fill('#1a9042');
-    rect(w/8,w/8,w,w);
-    
-    strokeWeight(3);
-    for(let i=0;i<9;i++){
-        line(w/8,w/8+w/8*i,w/8+w,w/8+w/8*i);
-        line(w/8+w/8*i,w/8,w/8+w/8*i,w/8+w);
-    }
-    for(let i=1;i<9;i++)    for(let j=1;j<9;j++){
-        if(board[i][j]==1){
-            fill(0);
-            circle(w/16+w/8*j,w/16+w/8*i,w/11);
-        }else if(board[i][j]==2){
-            fill(255);
-            circle(w/16+w/8*j,w/16+w/8*i,w/11);
+            break;
         }
     }
 
     noStroke();
-    textSize(25);
-    if(swi3&&!swi4){
-        textSize(30);
-        fill(0);
-        text("あなたの番です",50,30);
-    }else if(!swi4){
-        fill('#554499');
-        text("相手の番です",50,30);
-        
+    for(let i=0;i<17;i++)   for(let j=0;j<17;j++){
+        if(block[i][j]==1)  fill(150);
+        else if(block[i][j]>=8&&block[i][j]<=11) fill(col[block[i][j]-8]);
+        else    fill(255);
+        if(i%2==0&&j%2==0)  fill(200);
+        if(block[i][j]==7&&myturn){
+            fill("#ffcc88");
+            if(i==pc&&j==pr)    fill('#ff7700');
+        }
+        rect(ts*int((i+1)/2)+rs*int(i/2)+px,ts*int((j+1)/2)+rs*int(j/2)+py,(i%2)*rs+((i+1)%2)*ts,(j%2)*rs+((j+1)%2)*ts);
+
+        if(block[i][j]>=3&&block[i][j]<=6){
+            fill(col[block[i][j]-3])
+            circle(ts*int((i+1)/2)+rs*int(i/2)+ts/2+px,ts*int((j+1)/2)+rs*int(j/2)+ts/2+py,ts*0.7);
+        }
     }
 
-    if(swi4){
-        fill(0);
-        if(resp>rest)   text("ゲーム終了　くろの勝ち！！",50,30);
-            else    text("ゲーム終了　しろの勝ち！！",50,30);
-        text("くろ："+resp+"  しろ："+rest,50,60);
+    if(turn%2==0) strokeWeight(12);    else    strokeWeight(4);
+    stroke(col[pnum-1]);
+    line(px,py+9*ts+8*rs+10,px+9*ts+8*rs,py+9*ts+8*rs+10);
+    if(turn%2==1) strokeWeight(12);    else    strokeWeight(4);
+    stroke(col[(pnum)%2]);
+    line(px,py-10,px+9*ts+8*rs,py-10);
 
+
+    noStroke(),fill(0),textSize(30);
+    text(wall,800,800);
+}
+
+function mouseClicked(){
+    let flag=true;
+
+    if(myturn){
+        if(start==true)    start=false;
+        for(let i=0;i<9;i++)    for(let j=0;j<9;j++){
+            if(mouseX>i*(ts+rs)+px&&mouseX<i*(ts+rs)+ts+px&&mouseY>j*(ts+rs)+py&&mouseY<j*(ts+rs)+ts+py){
+                if(block[i*2][j*2]==7){
+                    block[i*2][j*2]=pnum+2;
+                    block[c][r]=0;
+                    room.send(pnum+',m,'+c+','+r+','+i*2+','+j*2);
+                    c=i*2;
+                    r=j*2;
+                    myturn=false;
+                    turn=(turn+1)%4;
+                    flag=false;
+                }
+                break;
+            }
+        }
+
+        if(flag)    for(let i=0;i<8;i++)   for(let j=0;j<8;j++){
+            if(dist(mouseX,mouseY,ts+i*(ts+rs)+rs/2+px,ts+j*(ts+rs)+rs/2+py)<ts/3){
+                if(block[i*2+1][j*2+1]==1&&block[i*2+1+dir[mode][0][0]][j*2+1+dir[mode][0][1]]==1
+                &&block[i*2+1+dir[mode][1][0]][j*2+1+dir[mode][1][1]]==1){
+                    block[i*2+1][j*2+1]=pnum+7;
+                    block[i*2+1+dir[mode][0][0]][j*2+1+dir[mode][0][1]]=pnum+7;
+                    block[i*2+1+dir[mode][1][0]][j*2+1+dir[mode][1][1]]=pnum+7;
+                    room.send(pnum+',w,'+mode+','+(i*2+1)+','+(j*2+1));
+                    myturn=false;
+                    wall--;
+                    turn=(turn+1)%4;
+                }
+                break;
+            }
+        }
     }
-    stroke(0);
-    
 }
 
-
- 
-function windowResized(){
-    resizeCanvas(windowWidth,windowHeight);
-    w=min(width,height)*0.8;
+function mouseWheel(){
+    mode=(mode+1)%2;
 }
 
-function makeid() {
-    var text = "";
-    var possible = "abcdefghijkmnopqrstuvwxyz023456789";
-  
-    for (var i = 0; i < 5; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-  
-    return text;
-}
+function enable(){
 
-function count(){
-    for(let i=1;i<9;i++)    for(let j=1;j<9;j++){
-        if(board[i][j]==1)  resp++;
-        if(board[i][j]==2)  rest++;
+    if(start==false){
+        let d=[[0,-1],[1,0],[0,1],[-1,0]];
+        for(let i=0;i<17;i++)   for(let j=0;j<17;j++)   if(block[i][j]==7)  block[i][j]=0;
+        for(let i=0;i<4;i++){
+            if(ins(c+d[i][0],r+d[i][1])){
+                if(block[c+d[i][0]][r+d[i][1]]==0){
+                    if(block[c+d[i][0]*2][r+d[i][1]*2]==0)  block[c+d[i][0]*2][r+d[i][1]*2]=7;
+                    else{
+                        if(ins(c+d[i][0]*3,c+d[i][1]*3)){
+                            if(block[c+d[i][0]*3][r+d[i][1]*3]==0&&block[c+d[i][0]*4][r+d[i][1]*4]==0)  block[c+d[i][0]*4][r+d[i][1]*4]=7;
+                            else    if(block[c+d[i][0]*3][r+d[i][1]*3]!=0){
+                                let cc=c+d[i][0]*2,rr=r+d[i][1]*2;
+                                for(let j=0;j<4;j++){
+                                    if(ins(cc+d[j][0],rr+d[j][1])){
+                                        if(block[cc+d[j][0]][rr+d[j][1]]==0&&block[cc+d[j][0]*2][rr+d[j][1]*2]==0)
+                                            block[cc+d[j][0]*2][rr+d[j][1]*2]=7;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-  
+
+function ins(cc,rr){
+    if(cc>=0&&cc<17&&rr>=0&&rr<17)  return true;
+    else    return false;
+}
+
+function receive(s){
+    cmd(s);
+}
+
+function cmd(s){
+    s=s.split(',');
+    s[0]=int(s[0]);
+    if(s[1]=='m'){
+        block[ conc(s[2],s[3],s[0]) ][ conr(s[2],s[3],s[0]) ]=0;
+        block[ conc(s[4],s[5],s[0]) ][ conr(s[4],s[5],s[0]) ]=s[0]+2;
+        enable();
+        turn++;
+    }
+    if(s[1]=='w'){
+        for(let i=2;i<5;i++)    s[i]=int(s[i]);
+        block[ conc(s[3],s[4],s[0]) ][ conr(s[3],s[4],s[0]) ]=s[0]+7;
+        if(s[2]==0){
+            block[ conc(s[3],s[4],s[0])-1 ][ conr(s[3],s[4],s[0]) ]=s[0]+7;
+            block[ conc(s[3],s[4],s[0])+1 ][ conr(s[3],s[4],s[0]) ]=s[0]+7;
+        }else{
+            block[ conc(s[3],s[4],s[0]) ][ conr(s[3],s[4],s[0])-1 ]=s[0]+7;
+            block[ conc(s[3],s[4],s[0]) ][ conr(s[3],s[4],s[0])+1 ]=s[0]+7;
+        }
+        enable();
+        turn++;
+    }
+
+    myturn=true;
+    return s;
+}
+
+function conc(c,r,n){
+    return 16-c;
+}
+
+function conr(c,r,n){
+    return 16-r;
+}
